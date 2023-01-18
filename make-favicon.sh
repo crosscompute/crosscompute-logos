cbd6ada667d638be79fcb15da820b0a5b29c178b
Roy Hyunjin Han <rhh@crosscompute.com>
2023-01-18 13:24:09 -0500

    Update favicon

--- make-favicon.sh
+++ make-favicon.sh
@@ -1,4 +1,5 @@
-convert Logo-SmallFormat-20220127-Star.svg \
+#!/bin/bash
+convert Logo-SmallFormat-20230118.svg \
   -transparent "#ffffff" \
   -resize 64x64  \
   -define icon:auto-resize="64,32,16" \
